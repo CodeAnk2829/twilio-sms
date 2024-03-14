3cc0ea8ba36c01d89396f7425c11879445d7f195
Ankit <211112004@stu.manit.ac.in>
2024-03-14 23:34:36 +0530

    feat(server): added notification

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require("twilio")(accountSid, authToken);
+const sender = process.env.TWILIO_FROM;
+
+app.post("/notify", async (req, res) => {
+
+const { recipients } = req.body;
+
+  recipients.forEach((recipient) => {
+    client.messages
+      .create({
+        body: "Hello from Maulana Azad National Insitute of Technolgy",
+        from: sender,
+        to: recipient,
+      })
+      .then((message) => {
+        console.log(message.sid);
+      })
+      .catch(() => {
+        throw new Error("error while sending sms");
+      });
+  });
+  return res.json({
+    msg: "ok",
+  });
+});
+
+app.listen(3000, console.log("Server is running"));
