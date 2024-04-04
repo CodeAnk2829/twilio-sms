92e42ab69aaab62f4e44d16649aec404210a9544
Ankit <211112004@stu.manit.ac.in>
2024-04-05 01:39:05 +0530

    fix(server): update cors policy

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const cors = require("cors");
 
 const app = express();
 
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
